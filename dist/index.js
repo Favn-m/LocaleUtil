e002c6d90c298d6a07a5b712f682d37134425276
Favn-m <farmineyt@gmail.com>
2022-12-27 14:44:52 +0200

    *

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,6 @@
-// import LocaleManager from "./managers/LocaleManager.js";
+import LocaleManager from "./managers/LocaleManager.js";
 import Locales from "./managers/Locales.js";
-// export default{
-//     LocaleManager,
-//     Locales,
-// }
-import { fileURLToPath } from 'url';
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-(async () => {
-    const locales = new Locales("file://" + __dirname + "/locales/");
-    // locales.setPath("file://"+__dirname+"locales/");
-    const ukrainian = await locales.getLocale('uk-UA');
-    const english = await locales.getLocale('uk-UA');
-    console.log({
-        ukrainian: ukrainian,
-        english: english
-    });
-})();
+export default {
+    LocaleManager,
+    Locales,
+};
