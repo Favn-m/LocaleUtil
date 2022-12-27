df1dc3bfdebde7045a10a4b260ba243274fe63ea
Favn-m <farmineyt@gmail.com>
2022-12-27 15:29:14 +0200

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,17 +1,19 @@
-export class LocaleManager{
-    constructor(locales: Locales, language: string);
-
-    public getString(key: string): string;
-    private getLocales(): Promise<object>;
-
-    public static getByLanguage(locales: Locales, language: string): Promise<LocaleManager>;
-}
-
-
-export class Locales{
-    constructor(path?: string);
-
-    public getString(language: string, key: string): Promise<string>;
-    public setPath(path: string): Locales;
-    public getLocale(language: string): Promise<LocaleManager>;
+declare module 'localeutil'{
+    export class LocaleManager{
+        constructor(locales: Locales, language: string);
+    
+        public getString(key: string): string;
+        private getLocales(): Promise<object>;
+    
+        public static getByLanguage(locales: Locales, language: string): Promise<LocaleManager>;
+    }
+    
+    
+    export class Locales{
+        constructor(path?: string);
+    
+        public getString(language: string, key: string): Promise<string>;
+        public setPath(path: string): Locales;
+        public getLocale(language: string): Promise<LocaleManager>;
+    }
 }
